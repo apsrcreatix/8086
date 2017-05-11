11b9e79a2980fcbfc1b2b5e74f4f38253e583101
Aditya Pratap Singh Rajput <apsrcreatix@gmail.com>
2017-05-12 02:11:59 +0530

    Addition

--- ADDITION.asm
+++ ADDITION.asm
@@ -1,9 +1,9 @@
 ASSUME CS : CODE , DS DATA
 CODE SEGMENT
     START : 
-    MOV AX,00FFH
+    MOV AX,2H
     MOV BX,3H
-    ADD AX,BX
+    ADD AX,BX ; addition of two registers and it stores the result in AX
     ENDS
 ENDS START
 
